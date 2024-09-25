8509ef38c132b1ef453e5b500a82af4c32d2c679
Oscar12233 <osacardunkeando@gmail.com>
2024-09-25 10:28:47 -0400

    primera version2

---
+++ app/src/main/java/com/example/ecoomerce_281/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ecoomerce_281
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
